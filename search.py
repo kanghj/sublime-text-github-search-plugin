741d4bf3a38bc656c29ca78ea9b83ecfa2247920
Kang HJ <kanghongjin@gmail.com>
2016-08-28 19:57:16 +0800

    context menu

--- search.py
+++ search.py
@@ -1,10 +1,6 @@
 import sublime, sublime_plugin
 import webbrowser
 
-class ExampleCommand(sublime_plugin.TextCommand):
-    def run(self, edit):
-        self.view.insert(edit, 0, "Hello, World!")
-
 class SearchGithubCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         for selection in self.view.sel():
