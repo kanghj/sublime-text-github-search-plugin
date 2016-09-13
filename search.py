85b6e843e0fad8fc3cd3a3bca1a92873a1a09c0b
Kang HJ <kanghongjin@gmail.com>
2016-09-13 23:14:02 +0800

    url encode search text

--- search.py
+++ search.py
@@ -1,6 +1,18 @@
 import sublime, sublime_plugin
 import webbrowser
 
+SUBLIME_3 = sublime.version() == '' or int(sublime.version()) > 3000
+
+if SUBLIME_3:
+    def encode(text):
+        from urllib.parse import quote_plus
+        return quote_plus(text)
+else:
+    def encode(text):
+        import urllib
+        return urllib.quote_plus(text)
+
+
 class SearchGithubCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         for selection in self.view.sel():
@@ -9,4 +21,4 @@ class SearchGithubCommand(sublime_plugin.TextCommand):
 
             text = self.view.substr(selection)
 
-            webbrowser.open_new_tab( 'https://github.com/search?q=%s&ref=simplesearch&type=Code' % (text, ) )
+            webbrowser.open_new_tab( 'https://github.com/search?q=%s&ref=simplesearch&type=Code' % (encode(text), ) )
