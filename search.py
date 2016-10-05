b0430d28b27bcd25f999a953370477e5b5e06995
Kang Hong Jin <kanghongjin@gmail.com>
2016-10-06 00:04:32 +0800

    json parse response

--- search.py
+++ search.py
@@ -1,5 +1,6 @@
 import sublime, sublime_plugin
 import webbrowser
+import json
 
 SUBLIME_3 = sublime.version() == '' or int(sublime.version()) > 3000
 PHANTOM_SUPPORT = int(sublime.version()) >= 3118
@@ -30,6 +31,12 @@ def github_token():
     except:
         return ''
 
+def user_email():
+    try:
+        plugin_settings = sublime.load_settings("GithubSearch.sublime-settings")
+        return plugin_settings.get('username')['github.com']
+    except:
+        return ''
 
 class SearchGithubIssueCommand(sublime_plugin.TextCommand):
     def run(self, edit):
@@ -43,6 +50,7 @@ class SearchGithubCodeCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         pset = sublime.PhantomSet(self.view)
         token = github_token()
+        
 
         for selection in self.view.sel():
             if selection.empty():
@@ -52,8 +60,11 @@ class SearchGithubCodeCommand(sublime_plugin.TextCommand):
             selected = self.view.substr(selection)
             print('https://api.github.com/search/code?q=' + encode(selected))
             req = Request('https://api.github.com/search/code?q=' + encode(selected))
+            req.add_header('Authorization', 'token ' + token)
             req.add_header('Accept', 'application/vnd.github.v3.text-match+json')
-            #resp = urlopen(req).read()
+            resp = urlopen(req).read()
+
+            github_data = json.loads(resp.decode('utf8', 'ignore'))
 
             self.view.show_popup('<div style="background-color:rgba(123,123,123);"> ' + str(5) + '</div>', sublime.HIDE_ON_MOUSE_MOVE_AWAY, -1, 480, 700)
 
